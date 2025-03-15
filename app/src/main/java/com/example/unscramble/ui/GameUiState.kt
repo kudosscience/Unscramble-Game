847a43531d100846bfe034eb5a7917f2b8d9729d
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2025-03-15 12:51:30 +0000

    Handle last round of game

--- app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -3,6 +3,7 @@ package com.example.unscramble.ui
 data class GameUiState(
     val currentScrambledWord: String = "",
     val currentWordCount: Int = 1,
+    val score: Int = 0,
     val isGuessedWordWrong: Boolean = false,
-    val score: Int = 0
+    val isGameOver: Boolean = false
 )
